bb0d203a089f12602823aca8dd9894feb090b696
nayem73 <nayem73@gmail.com>
2023-10-25 08:01:55 +0600

    Beginning Docker training with Spring Boot

---
+++ Docker Training/practicedocker/src/test/java/com/nayem/practicedocker/PracticedockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nayem.practicedocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticedockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
